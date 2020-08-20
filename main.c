e01f010240ae1c28918111106e1db1582a42d87a
TheCasProg <68574716+TheCasProg@users.noreply.github.com>
2020-08-19 19:51:49 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,1 +1,117 @@
-TBA
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+
+FILE * fptr;
+FILE * fptr2;
+
+#define MAX 256
+
+char buff[100];
+
+struct custInfo
+{
+    int ID;
+    char name[50];
+    char phone[50];
+    double billing;
+};
+
+void add()
+{
+    fptr = fopen ("C:/Users/Tox/Documents/C C++ Projects/Practice Projects/CTelecom/bin/Data/src.txt", "a");
+    struct custInfo user;
+    printf("ID:\n");
+    scanf ("%d", &user.ID);
+    getchar();
+    printf("Name:\n");
+    gets (user.name);
+    printf("Phone:\n");
+    scanf ("%s", user.phone);
+    getchar();
+    printf("Billing:\n");
+    scanf ("%lf", &user.billing);
+    getchar();
+    fprintf (fptr, "%d %s %s %.2lf \n", user.ID, user.name, user.phone, user.billing);
+    fclose (fptr);
+}
+
+
+void search()
+{
+
+    fptr = fopen ("C:/Users/Tox/Documents/C C++ Projects/Practice Projects/CTelecom/bin/Data/src.txt", "r");
+    while (fgets(buff, sizeof(buff), fptr))
+    {
+        printf("%s\n", buff);
+    }
+    fclose(fptr);
+    getchar();
+
+}
+
+void deleteData()
+{
+    char fname[MAX];
+    char str[MAX];
+    char ch;
+    int loca, ctr = 0;
+    fptr = fopen ("C:/Users/Tox/Documents/C C++ Projects/Practice Projects/CTelecom/bin/Data/src.txt", "r");
+    fptr2 = fopen ("C:/Users/Tox/Documents/C C++ Projects/Practice Projects/CTelecom/bin/Data/src2.txt", "w");
+    printf("Line: ");
+    scanf("%d", &loca);
+    getchar();
+    while (!feof(fptr))
+        {
+            strcpy(str, "\0");
+            fgets(str, MAX, fptr);
+            if (!feof(fptr))
+            {
+                ctr++;
+                if (ctr != loca)
+                {
+                    fprintf(fptr2, "%s", str);
+                }
+            }
+        }
+    fclose(fptr);
+    fclose(fptr2);
+    remove("C:/Users/Tox/Documents/C C++ Projects/Practice Projects/CTelecom/bin/Data/src.txt");
+    rename("C:/Users/Tox/Documents/C C++ Projects/Practice Projects/CTelecom/bin/Data/src2.txt", "C:/Users/Tox/Documents/C C++ Projects/Practice Projects/CTelecom/bin/Data/src.txt");
+}
+
+int main()
+{
+    int exit = 0;
+    while (exit != 1)
+    {
+        system("cls");
+        printf(" A - Add \n S - Search \n D - Delete \n X - Leave \n");
+        printf ("Enter Command: ");
+        char dec = 'X';
+        scanf("%c", &dec);
+        getchar();
+        switch(toupper(dec))
+        {
+        case 'A':
+            add();
+            break;
+        case 'S':
+            search();
+            break;
+        case 'D':
+            deleteData();
+            break;
+        case 'X':
+            exit = 1;
+            break;
+        default :
+            printf ("%c: Is an unknown command", dec);
+            sleep(1);
+            continue;
+        }
+
+    }
+    return 0;
+}
